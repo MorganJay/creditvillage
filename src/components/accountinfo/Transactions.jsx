8bd772c302b683816a837f28685e441b168e9678
James Morgan <MorganJay@users.noreply.github.com>
2021-09-06 22:46:28 +0100

    completed transactions page

---
+++ src/components/accountinfo/Transactions.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { ListWrapper } from "./AccountDetails";
+
+const Transactions = () => {
+  return (
+    <ListWrapper className="pr-3">
+      <li>
+        VALUE ADDED TAX VAT ON NIP TRANSFER...<span>NGN 3.75</span>
+      </li>
+      <li>
+        COMMISSION NIP TRANSFER COMMISSION ...<span>NGN 50.00</span>
+      </li>
+      <li>
+        NIBSS Instant Payment Outward Yusuf...<span>NGN 100,000.00</span>
+      </li>
+      <li>
+        VALUE ADDED TAX VAT ON NIP TRANSFER...<span>NGN 1.87</span>
+      </li>
+      <li>
+        COMMISSION NIP TRANSFER COMMISSION ...<span>NGN 25.00</span>
+      </li>
+      <li>
+        NIBSS Instant Payment Outward Borro...<span>NGN 34,000.00</span>
+      </li>
+      <li>
+        COMMISSION NIP TRANSFER COMMISSION ...<span>NGN 35,365.53</span>
+      </li>
+      <li>
+        COMMISSION NIP TRANSFER COMMISSION ...<span>NGN 35,365.53</span>
+      </li>
+      <li>
+        COMMISSION NIP TRANSFER COMMISSION ...<span>NGN 35,365.53</span>
+      </li>
+      <li>
+        COMMISSION NIP TRANSFER COMMISSION ...<span>NGN 35,365.53</span>
+      </li>
+    </ListWrapper>
+  );
+};
+
+export default Transactions;
