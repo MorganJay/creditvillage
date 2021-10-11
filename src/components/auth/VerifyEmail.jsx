5e9d9c2ca63188efdcfae976f42060e83e0b8d83
James Morgan <MorganJay@users.noreply.github.com>
2021-10-11 16:37:45 +0100

    completed verify email screen

--- src/components/auth/VerifyEmail.jsx
+++ src/components/auth/VerifyEmail.jsx
@@ -5,7 +5,7 @@ import GlassModal from "../modals/GlassModal";
 import CreditButton from "../buttons/Button";
 import PinInput from "../inputs/PinInput";
 
-const VerifyEmail = ({ history }) => {
+const VerifyEmail = ({ history, email }) => {
   const [otp, setOtp] = useState("");
 
   const handleOtpChange = e => {
@@ -19,17 +19,15 @@ const VerifyEmail = ({ history }) => {
   };
 
   return (
-    <GlassModal style={{ padding: "3rem 1rem" }}>
-      <h1
-        style={{
-          color: "white",
-          WebkitTextFillColor: "white",
-          marginBottom: "3rem",
-        }}
-      >
-        Verify your email
-      </h1>
-      <Container>
+    <Container className="w-100 d-flex flex-column flex-lg-row justify-content-between h-100">
+      <Title>
+        <h1 className="font-weight-semi-bold">Enter the Code sent to</h1>
+        <p>{email || "maryagatha@gmail.com"}</p>
+        <p className="font-weight-semi-bold" onClick={() => history.goBack()}>
+          Wrong email?
+        </p>
+      </Title>
+      <VerifyModal className="align-self-center">
         <PinInput
           name="emailOtp"
           value={otp}
@@ -37,24 +35,62 @@ const VerifyEmail = ({ history }) => {
           placeholder="______"
           onChange={handleOtpChange}
         />
-      </Container>
-      <p>Enter 6-digit Pin sent to your email</p>
-      <CreditButton
-        styles={{ fontSize: "20px", fontWeight: "600", marginTop: '2rem' }}
-        onClick={verifyEmail}
-        inverted
-      >
-        Verify Email
-      </CreditButton>
-      <br />
-    </GlassModal>
+        <p>Enter 6-digit OTP sent to your email</p>
+        <EmailActions className="d-flex w-100 justify-content-between align-items-center mt-3">
+          <p className="m-0">
+            Didn't get the OTP? <span>Resend</span>
+          </p>
+          <Button onClick={verifyEmail} inverted>
+            Verify
+          </Button>
+        </EmailActions>
+      </VerifyModal>
+    </Container>
   );
 };
 
 export default VerifyEmail;
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
+  padding: 5rem 3rem 3rem;
+`;
+
+const Title = styled.div`
+  padding: 4rem 0;
+  margin-left: 5%;
+  h1 {
+    color: var(--lightblue);
+    font-size: 2.25rem;
+    font-family: "CamptonMedium";
+  }
+  p {
+    color: var(--lighterblue);
+    font-size: 1.5rem;
+    &:last-child {
+      color: var(--darkblue);
+      cursor: pointer;
+      width: fit-content;
+    }
+  }
+`;
+
+const VerifyModal = styled(GlassModal)`
+  padding: 5rem 2rem;
+  grid-gap: 1rem;
+  max-width: 712px;
+  max-height: 400px;
+  width: fit-content;
+`;
+
+const EmailActions = styled.div`
+  span {
+    color: var(--darkblue);
+    cursor: pointer;
+  }
+`;
+
+const Button = styled(CreditButton)`
+  font-size: 18px;
+  border-radius: 50px;
+  line-height: 21px;
 `;
--- src/components/inputs/PinInput.jsx
+++ src/components/inputs/PinInput.jsx
@@ -1,50 +1,46 @@
-import React from 'react';
-import styled from 'styled-components';
-import OtpInput from 'react-otp-input';
+import React from "react";
+import styled from "styled-components";
+import OtpInput from "react-otp-input";
 
-const PinInput = ({ ...props }) => {
-  return (
-    <>
-      <StyledOtpInput
-        shouldAutoFocus
-        className='otp-input'
-        otpType='password'
-        autoComplete='off'
-        disabled={false}
-        secure={true}
-        separator={<span> </span>}
-        isInputSecure
-        {...props}
-      />
-    </>
-  );
-};
+const PinInput = ({ ...props }) => (
+  <StyledOtpInput
+    shouldAutoFocus
+    className="otp-input"
+    otpType="password"
+    autoComplete="off"
+    disabled={false}
+    secure={true}
+    separator={<span> </span>}
+    isInputSecure
+    {...props}
+  />
+);
 
 export default PinInput;
 
 const StyledOtpInput = styled(OtpInput)`
   input {
-    width: 3em !important;
+    width: 4.25em !important;
+    height: 4.8em !important;
     text-align: center;
     color: var(--darkblue);
     border: none;
-    margin: 10px 15px;
+    margin: 10px;
     height: 54px;
     outline: none;
-    padding: 15px 20px;
+    padding: 15px;
     background: linear-gradient(
       275.85deg,
       #d9e8ef -22.37%,
       rgba(217, 232, 239, 0) 124.27%
     );
     box-shadow: inset 0px 4px 10px rgba(221, 221, 221, 0.12);
-    border-radius: 4px;
     font-style: normal;
     font-weight: normal;
     font-size: 20px;
     line-height: 24px;
     text-shadow: 0px 4px 10px rgba(78, 101, 128, 0.12);
-    
+
     &::placeholder {
       color: var(--darkblue);
       opacity: 1;
